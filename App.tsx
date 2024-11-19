07adae8c90410c15dd7357f7ad0edbf8e0e634ff
hauraa2024 <haurafadilla16@gmail.com>
2024-11-19 08:29:29 +0700

    small changes in README.md

--- App.tsx
+++ App.tsx
@@ -75,6 +75,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    backgroundColor: 'black',
   },
   formContainer: {
     alignItems: 'center',
